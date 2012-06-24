39b17970c138867fb5bdc173bc25e6a2b820878a
Gabriel <gabrielfigueroa06@gmail.com>
2012-06-24 12:32:15 -0300

    boludes persona sin mail

--- lib/form/PersonaForm.class.php
+++ lib/form/PersonaForm.class.php
@@ -11,13 +11,15 @@ class PersonaForm extends BasePersonaForm
 {
   public function configure()
   {
-      //$this->widgetSchema['e_mail']->setDefault("persona@dominio.com");
-      $this->validatorSchema['e_mail']->setOption('required', false);
-      $this->widgetSchema['fecha_nacimiento']->setDefault("06-06-2006");
       $this->widgetSchema['nro_doc']= new sfWidgetFormInputText();
+      $this->validatorSchema['nro_doc'] = new sfValidatorInteger();
+      
 	  $this->widgetSchema['fecha_nacimiento'] = new sfWidgetFormInputText();
-	  $this->validatorSchema['nro_doc'] = new sfValidatorInteger(); 
+	  //$this->validatorSchema['fecha_nacimiento'] = new sfValidatorDate();
+	  $this->widgetSchema['fecha_nacimiento']->setDefault("06-06-2006");
+	   
 	  $this->validatorSchema['e_mail'] = new sfValidatorEmail(); 
+	  $this->validatorSchema['e_mail']->setOption('required', false);
 	
 	}
 }
