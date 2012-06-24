406462330261186ea417d47898f01d6fe38e57e4
Gabriel <gabrielfigueroa06@gmail.com>
2012-06-24 13:42:40 -0300

    embebiendo formulario pero no anda como kiero

--- lib/form/ProductoForm.class.php
+++ lib/form/ProductoForm.class.php
@@ -11,8 +11,19 @@ class ProductoForm extends BaseProductoForm
 {
   public function configure()
   {
-
-	        $this->widgetSchema['descripcion_prod']->setDefault("caca");
+		$this->widgetSchema['id']= new sfWidgetFormInputText();
+		
+		parent::configure();       
+ 
+        $child = new Stock();
+        $child->Parent= $this->getObject();
+ 
+        $form2 = new StockForm($child);
+ 
+        $this->embedForm('child', $form2);
+        
+       //esto anda,,, pasa q no guarda en la tabla stock pq no existe
+       // la referencia de un producto q aun no esta creado
 
 
 
